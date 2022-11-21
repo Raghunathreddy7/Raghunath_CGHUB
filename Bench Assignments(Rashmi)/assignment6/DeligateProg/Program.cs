783581a97d6f546907314b275acc5e19c7387da1
Raghunathreddy7 <raghunathreddy450@gmail.com>
2022-11-21 12:56:54 +0530

    first commit

---
+++ Bench Assignments(Rashmi)/assignment6/DeligateProg/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeligateProg
+{
+    internal class Program
+    {
+        public delegate void DelegateDemo();
+
+        public delegate void DelegateDemo2(string msg);
+        static void Main(string[] args)
+        {
+
+            // ------------------ EMPLOYEE MANAGEMENT CODE STARTS --------------------------------
+            // Task 6 [date = 15-11-2022]
+
+            Console.WriteLine("Welcome to Litware Application!");
+            Employee e1 = new Employee(1, "Raghu", 30000, "employee", 0);
+
+
+            // Instantiate delegate (multicast delegate)
+            DelegateDemo h1 = e1.CalculateSalary;
+            DelegateDemo h2 = e1.EmpDetails;
+            DelegateDemo alldelegates = h1 + h2;
+
+
+            // Instantiate delegate (Unicast delegate)
+            DelegateDemo2 greetings = e1.PrintMessage;
+
+
+            //call the delegates
+            alldelegates();
+
+            greetings("------------- Bye --------------");
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ Bench Assignments(Rashmi)/assignment6/assignment6/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment6
+{
+    internal class Program
+    {
+        public delegate void Transactions(double money);
+
+        public delegate void CheckMessages();
+
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("------------ Hello, welcome to CHK Banking ---------------");
+
+            Account chk = new Account(12345, "Karthik");
+
+            // Initialize the delegates
+            Transactions deposit = chk.Deposit;
+            Transactions withdraw = chk.Withdraw;
+
+            CheckMessages cm1 = chk.ZeroBalanceMessage;
+            CheckMessages cm2 = chk.UnderBalance;
+            CheckMessages chkmessages = cm1 + cm2;
+
+            deposit(1700);
+            withdraw(1300);
+            chkmessages();
+
+
+        }
+    }
+}
